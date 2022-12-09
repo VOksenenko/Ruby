9e147dfcefbaa461e4892b7f79e741ab97f18925
Vlad Oksenenko <voksenenko@gmail.com>
2022-12-09 22:36:07 +0200

    pizza with d

---
+++ pizza_with_d.rb
@@ -0,0 +1,21 @@
+class Pizza
+  @@count = 0
+  
+  def initialize(d, ingredients)
+    @d = d
+    @ingredients = []
+    @number = 0
+    if @d == 25 or @d == 50
+      @@count += 1
+      @number = @@count
+      @ingredients = ingredients
+    else
+      @d = 0
+    end
+  end
+
+  def description
+    ingredients = @ingredients.map { |i| i }.join(",")
+    "Піца № #{@number} (діагональ = #{@d}) містить [#{ingredients}]."
+  end
+end
